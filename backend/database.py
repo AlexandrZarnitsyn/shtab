a4767def720c30cce4ecc33d1a0fb17f3409e23a
AlexandrZarnitsyn <139579941+AlexandrZarnitsyn@users.noreply.github.com>
2026-04-15 13:13:31 +0300

    Add files via upload

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+else:
+    engine = create_engine(
+        "sqlite:///./shtab.db",
+        connect_args={"check_same_thread": False}
+    )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
